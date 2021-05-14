a3fff367acaa4ade35ea23e630fd88072014a7e2
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2021-05-14 17:41:29 +0300

    feels like git pro

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { List } from 'semantic-ui-react';
+import {Activity} from '../models/activity'
+import NavBar from './NavBar';
+
+function App() {
+  const [activities,setActivities]=useState<Activity[]>([]);
+  useEffect(()=>{
+    axios.get<Activity[]>('http://localhost:3000/FiratAPI/activities').then(response=>{
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, []);
+  return (
+    <div >
+      <NavBar />       
+        <List>
+          {activities.map((activity)=>(
+            <List.Item key={activity.id} >
+              {activity.title }
+              </List.Item>
+          ))}
+        </List>
+          
+        
+      
+    </div>
+  );
+}
+
+export default App;
